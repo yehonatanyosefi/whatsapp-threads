df3308efe5f407fb2e346a7a552c52f7fd235213
Yehonatan Lavy <yehonatanmind@gmail.com>
2024-12-06 21:13:16 +0200

    added vercel's web analyticsz

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
 import { Toaster } from '@/components/ui/sonner'
 import './globals.css'
-
+import { Analytics } from '@vercel/analytics/next'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<body>
 				{children}
 				<Toaster />
+				<Analytics />
 			</body>
 		</html>
 	)
